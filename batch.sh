d6fd0174d8fb6ad7f531c76a49702d2d27aec1dc
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2024-12-18 23:18:05 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -2,7 +2,7 @@ memory_pressure -Q
 sleep 10
 ollama pull $1 1> /dev/null 2> /dev/null
 sleep 10
-pip3 install requests --break-system-packages
+pip3 install requests --break-system-packages 1> /dev/null 2> /dev/null
 time python3 script.py $1
 sleep 10
 ollama rm $1
