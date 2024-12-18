6c13b743b03b5969db02b1b8c15a2f603e08049c
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2024-12-18 14:08:20 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,5 +1,5 @@
 ollama pull $1 &> /dev/null
-time curl http://localhost:11434/v1/chat/completions \
+time curl -s http://localhost:11434/v1/chat/completions \
     -H "Content-Type: application/json" \
     -d "{
         \"model\": \"$1\",
