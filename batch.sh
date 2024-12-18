6af7d89b0bcbce1042905b86fd8a04f84a9c4d00
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2024-12-19 00:54:31 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,9 +1,18 @@
-#memory_pressure -Q
 sleep 10
 ollama pull $1 1> /dev/null 2> /dev/null
 sleep 10
-pip3 install requests --break-system-packages 1> /dev/null 2> /dev/null
-time python3 script.py $1
+time curl -s http://localhost:11434/v1/chat/completions \
+    -H "Content-Type: application/json" \
+    -d "{
+        \"model\": \"$1\",
+        \"messages\": [
+            {
+                \"role\": \"user\",
+                \"content\": \"富士山の高さを教えてください。\"
+            }
+        ],
+        \"max_tokens\": 1024
+    }" | jq .choices[0].message.content
 sleep 10
 ollama rm $1
 sleep 10
