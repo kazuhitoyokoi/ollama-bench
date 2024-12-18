8a4e557947ea5b768cff1c8a5b52d73a6fafdd7c
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2024-12-18 13:52:09 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -1,18 +1,18 @@
-ollama pull $1
+ollama pull $1 &> /dev/null
 time curl http://localhost:11434/v1/chat/completions \
     -H "Content-Type: application/json" \
-    -d '{
-        "model": "$1",
-        "messages": [
+    -d "{
+        'model': '$1',
+        'messages': [
             {
-                "role": "system",
-                "content": "You are a helpful assistant."
+                'role': 'system',
+                'content': 'You are a helpful assistant.'
             },
             {
-                "role": "user",
-                "content": "Hello!"
+                'role': 'user',
+                'content': 'Hello!'
             }
         ],
-        "max_tokens": 1024
-    }'
+        'max_tokens': 1024
+    }"
 ollama rm $1
