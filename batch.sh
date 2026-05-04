e7b52f507465394466029cabe321312bd4244f1f
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-05-04 12:22:37 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -3,8 +3,8 @@ sleep 10
 ollama pull $1 1> /dev/null 2> /dev/null
 sleep 10
 
-echo --- >> logs.txt
-echo $1 start >> logs.txt
+mkdir -p logs
+echo --- >> logs/$1.txt
 
 time curl -s http://localhost:11434/v1/chat/completions \
     -H "Content-Type: application/json" \
@@ -17,12 +17,10 @@ time curl -s http://localhost:11434/v1/chat/completions \
             }
         ],
         \"max_tokens\": 1024
-    }" | jq .choices[0].message.content >> logs.txt
+    }" | jq .choices[0].message.content >> logs/$1.txt
 sleep 10
 ollama rm $1
 sleep 10
-
-echo $1 end >> logs.txt
 
 git pull
 git config --global user.email "kazuhitoyokoi@nodered.jp"
