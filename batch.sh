928166e62a66d5dd31fe4b8140a390f9f2e93e93
Kazuhito Yokoi <kazuhitoyokoi@nodered.jp>
2026-05-04 12:03:36 +0900

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -13,16 +13,14 @@ time curl -s http://localhost:11434/v1/chat/completions \
             }
         ],
         \"max_tokens\": 1024
-    }" | jq .choices[0].message.content
+    }" | jq .choices[0].message.content >> logs.txt
 sleep 10
 ollama rm $1
 sleep 10
 
-echo $1 >> models.txt
-
 git pull
 git config --global user.email "kazuhitoyokoi@nodered.jp"
 git config --global user.name "Kazuhito Yokoi"
-git add models.txt
+git add logs.txt
 git commit -m "Update catalogue $1"
 git push
